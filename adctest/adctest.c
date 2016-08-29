cc9da93c7576aa77a10b2fa0cd4e736800851c5e
flatfeetmike <c9@zenmai.tk>
2016-08-29 06:02:07 +0000

    adc + uart worked

--- adctest/adctest.c
+++ adctest/adctest.c
@@ -48,10 +48,26 @@
 // Definitions
 #define _XTAL_FREQ 16000000      // this is used by the __delay_ms(xx) and __delay_us(xx) functions
 
+//**********************************************************************************
+// This subroutine does the ADC conversion and returns the 10 bit result
+//**********************************************************************************
+unsigned int Read_ADC_Value(void)
+{
+    unsigned int ADCValue;
+    
+    ADCON0bits.GO = 1;      // start conversion
+    while(ADCON0bits.GO);   // wait for conversion to finish
+    ADCValue = ADRESH << 8; // get the 2 msbs of the result and rotate 8 bits to the left
+    ADCValue = ADCValue + ADRESL;   // now add the low 8 bits of the resut into our return variable
+    return (ADCValue);      // return the 10bit result in a single variable
+}
+
+//**********************************************************************************
+//*****************   main routine   ***********************************************
+//**********************************************************************************
 void main(void) {
     unsigned char portValue;    // always use a variable to hold the value you want the port to assume
-    int i;
-
+    unsigned char ch[4];
     unsigned int AnalogValue;       // used to store ADC result after capture
 
     // set up oscillator control register
@@ -59,12 +75,22 @@ void main(void) {
     OSCCONbits.IRCF   = 0b1111; // set OSCCON IRCF bits to select OSC frequency = 16 MHz HF
     OSCCONbits.SCS    = 0b10;   // select internal oscillator block regardless of FOSC
 
-    // port A access
-    ANSELA = 0;                 // set to digital I/O (not analog)
-    TRISA = 0;                  // set all port bits to be output (except ra3)
-    PORTA = 0;                  // initialize the port
+    // set up port A I/O pins
+    TRISAbits.TRISA0 = 0;	// RA0 = nc
+    TRISAbits.TRISA1 = 0;	// RA1 = nc
+    TRISAbits.TRISA2 = 1;	// RA2 = Analog Voltage In
+    TRISAbits.TRISA3 = 0;	// RA3 = nc (MCLR)
+    TRISAbits.TRISA4 = 0;	// RA4 = nc
+    TRISAbits.TRISA5 = 0;	// RA5 = nc
 
-    // select alternative pin function
+    DACCON0bits.DACEN = 0;  // turn DAC off
+    ANSELAbits.ANSA2 = 1;	// Select RA2 as analog input pin for potentiometer input
+    ADCON0bits.CHS = 0x02;	// select RA2 analog channel
+    ADCON0bits.ADON = 1;    // ADC is on
+    ADCON1bits.ADCS = 0x01;	// select ADC conversion clock select as Fosc/8
+    ADCON1bits.ADFM = 1;    // results are right justified
+ 
+    // select alternative pin functions
     RXDTSEL = 1;                // select RA5 as rx port
     TXCKSEL = 1;                // select RA4 as tx port
 
@@ -72,20 +98,36 @@ void main(void) {
     RCSTA = 0b10010000;         // (*SPEN RX9  SREN *CREN  ADEN   FERR OERR RX9D)
     TXSTA = 0b00100100;         // ( CSRC TX9 *TXEN  SYNC  SENDB *BRGH TRMT TX9D)
     // set baudrate to 9600 bps
+    // baudrate = fosc / [16 (n + 1)]
     SPBRG = 103;                 // 9615 actual (0.16% error) 
 
-    __delay_ms(1000);
-    i = 0x30;
+    __delay_ms(500);
 
     while(1) {
-        while(TXIF == 0);       // block until tx buffer clears
-        TXREG = i;              // send the octet
+        AnalogValue = Read_ADC_Value();
         
-        i++;
-        if(i > 0x7b) {
-            i = 0x30;
-        }
-        __delay_ms(100);
+        ch[0] = AnalogValue % 10 + '0';   // 10^0 digit
+        AnalogValue = AnalogValue / 10;
+        ch[1] = AnalogValue % 10 + '0';   // 10^1 digit
+        AnalogValue = AnalogValue / 10;
+        ch[2] = AnalogValue % 10 + '0';   // 10^2 digit
+        AnalogValue = AnalogValue / 10;
+        ch[3] = AnalogValue % 10 + '0';   // 10^3 digit
+        
+        while(TXIF == 0);           // block until tx buffer clears
+        TXREG = ch[3];              // send the octet
+        while(TXIF == 0);
+        TXREG = ch[2];
+        while(TXIF == 0);
+        TXREG = ch[1];
+        while(TXIF == 0);
+        TXREG = ch[0];
+        while(TXIF == 0);
+        TXREG = '\r';
+        while(TXIF == 0);
+        TXREG = '\n';
+
+        __delay_ms(200);
     }
     return;                     // we should never reach this
     // jumps back to reset vector
